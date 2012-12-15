52cfae536a3ebe9482cdf1ce08ddd9435778c1ad
Josh Douglass-Molloy <joshuadm@gmail.com>
2012-12-15 19:56:28 +1030

    added LICENSE and fancied up README

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+--[[
+		main.lua - main loop and callbacks for SDIORETSA
+		
+		A game for Ludum Dare 25 by Josh Douglass-Molloy
+		
+		Released under the MIT License
+]]--
 function love.load()
 	entities = {1}
 	UNIVERSE_WIDTH = 1000
@@ -9,22 +16,34 @@ function love.load()
 end
 
 function love.update(dt)
-	--update motion, wrap around screen if applicable
+-- update motion, wrap around screen if applicable
 --	for i =1, #entities do
 	--	moveEntity(entities[i])
 --end
 end
 
 function love.draw()
-
+	
 end
 
 --draws an equilateral triangle centered on a 2d point
---can rotate
-function drawTriangle(x, y, size, rot)
-
+-- l defines the length of a side
+function drawTriangle(x, y, l, rot)
+	h = sqrt(3)/2*l
+	--vertices: top, left, right
+	verts = {x, y + h/2, x - l/2, y - h/2, x + l/2, y - h/2}  
+	--rotate triangle around centre
+	love.graphics.polygon('line', verts)
 end
-
+--[[
+	SHOOTAN ASTEROIDS
+	Player has an asterbar, divided into quarters: 
+		-4 bars allow the launching of a giant asteroid 
+		-3 bars allow the launching of a large asteroid (giant/2)
+		-2 bars allow the launching of a medium asteroid (large/2)
+		-1 bars allow the launching of a small asteroid (medium/2)
+	When destroyed, an asteroid will yield two asteroids of the next largest size
+]]--
 --creates the geometry of an asteroid and endows it with direction - a janky polygon (octogon)
 function makeAsteroids(size, direction)
 --make an octogon with length size on a side, moving in direction 
@@ -81,7 +100,7 @@ function moveEntity(e)
 	e.pos.x = canMod(e.pos.x + e.velocity.x, UNIVERSE_WIDTH)
 	e.pos.y = canMod(e.pos.y + e.velocity.y, UNIVERSE_HEIGHT)
 end
-
+--dot product of two vectors
 function dot(x0, x1, y0, y1)
 	return x0*x1 + y0*y1 
 end
